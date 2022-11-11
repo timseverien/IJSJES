a60dc12a0b7f737de302b3af21439c2ba434d5cc
Tim Severien <timseverien@gmail.com>
2022-11-11 15:51:52 +0100

    Add more Mastodon links

--- src/site.ts
+++ src/site.ts
@@ -5,8 +5,10 @@ export const SCHEMA_AUTHOR = {
 	'@type': 'Person',
 	name: 'Tim Severien',
 	sameAs: [
+		'https://mastodon.social/@ijsjes',
+		'https://mastodon.social/@timsev',
+		'https://timseverien.com',
 		'https://twitter.com/ijsje_dev',
 		'https://twitter.com/timseverien',
-		'https://timseverien.com',
 	],
 };
